549cfb406d8d604ca3fa0db0e3140c4c8321a6fd
bekzod@me.com <bekzod@me.com>
2015-12-16 16:41:03 +0500

    using addon option to import bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,14 +4,11 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    'ember-slack-search-input': {
+      importBootstrapCSS: true,
+      //importBootstrapJS: true,
+    }
   });
-
-  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css')
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.eot', { destDir: '/fonts' });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.svg', { destDir: '/fonts' });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.ttf', { destDir: '/fonts' });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff', { destDir: '/fonts' });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff2', { destDir: '/fonts'});
 
   return app.toTree();
 };
