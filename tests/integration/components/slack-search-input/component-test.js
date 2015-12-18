3dce18d90f46b3e279efe97fe001eaf2b5866012
bekzod@me.com <bekzod@me.com>
2015-12-18 15:04:02 +0500

    added basic test to main component

--- tests/integration/components/slack-search-input/component-test.js
+++ tests/integration/components/slack-search-input/component-test.js
@@ -5,10 +5,54 @@ moduleForComponent('slack-search-input', 'Integration | Component | slack search
   integration: true
 });
 
+const configHash = {
+ "before:": {
+   type: 'date',
+   defaultHint: 'a date',
+ },
+
+ "category:": {
+   type: 'list',
+   defaultHint: 'type',
+   sectionTitle: 'Action Types',
+   content: [
+     {value: 'animal', label: 'animal'},
+     {value: 'plant', label: 'plant'},
+     {value: 'mineral', label: 'mineral'}
+   ]
+ }
+};
+
 test('it renders', function(assert) {
   this.render(hbs`
     {{slack-search-input}}
   `);
+  assert.equal(this.$('input.slack-search-input')[0]);
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('modifiers highlighted', function(assert) {
+  this.set('inputValue', '');
+  this.set('configHash', configHash);
+  this.render(hbs`
+    {{slack-search-input
+      configHash=configHash
+      inputValue=inputValue}}
+  `);
+
+  this.set('inputValue', 'before:');
+  assert.equal(this.$('input').val(), 'before:');
+  assert.equal(this.$('.background-container .modifier.incomplete').text().trim(), 'before:');
+
+  this.set('inputValue', 'before:2000-12-12');
+  assert.equal(this.$('input').val(), 'before:2000-12-12');
+  assert.ok(!this.$('.background-container .modifier').hasClass('incomplete'));
+
+  this.set('inputValue', 'category:');
+  assert.equal(this.$('input').val(), 'category:');
+  assert.equal(this.$('.background-container .modifier.incomplete').text().trim(), 'category:');
+
+  this.set('inputValue', 'category:animal');
+  assert.equal(this.$('input').val(), 'category:animal');
+  assert.ok(!this.$('.background-container .modifier').hasClass('incomplete'));
+
 });
