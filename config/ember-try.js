136e49dfdcaa8cf01c9b33f379d77b47334b1a33
bekzod <bekzod@me.com>
2016-03-16 15:18:09 +0500

    updated libs

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,9 +1,46 @@
-/*jshint node:true*/
+/* jshint node: true */
+
 module.exports = {
   scenarios: [
     {
       name: 'default',
       dependencies: { }
+    },
+    {
+      name: 'ember-2.0',
+      dependencies: {
+        'ember': '~2.0.0'
+      },
+      resolutions: {
+        'ember': '~2.0.0'
+      }
+    },
+    {
+      name: 'ember-2.1',
+      dependencies: {
+        'ember': '~2.1.0'
+      },
+      resolutions: {
+        'ember': '~2.1.0'
+      }
+    },
+    {
+      name: 'ember-2.2',
+      dependencies: {
+        'ember': '~2.2.0'
+      },
+      resolutions: {
+        'ember': '~2.2.0'
+      }
+    },
+    {
+      name: 'ember-2.3',
+      dependencies: {
+        'ember': '~2.3.0'
+      },
+      resolutions: {
+        'ember': '~2.3.0'
+      }
     },
     {
       name: 'ember-release',
